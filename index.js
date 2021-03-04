d11707a4cd049bbfccae9cd2f55ccc79903fce2a
Jordan <jlewis2008@live.com>
2021-03-04 16:11:33 -0500

    rearranged order

--- index.js
+++ index.js
@@ -31,13 +31,6 @@ app.use(limiter);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.resolve(__dirname, "client/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  })
-}
-
 app.route("/").get(function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 })
@@ -46,6 +39,13 @@ app.use('/api/sessions', sessionRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/users', userRouter);
 app.use(errorController);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.resolve(__dirname, "client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  })
+}
 
 const PORT = process.env.PORT || 5000
 const server = app.listen(PORT, () => {
